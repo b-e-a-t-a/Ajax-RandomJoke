b468f86ee558bf459cd3ff582b7fdb628a61f017
Beata Karolczak <karolczak.beata@gmail.com>
2018-01-19 22:04:18 +0100

    Add jQuery method

--- js/script.js
+++ js/script.js
@@ -17,3 +17,26 @@ function getJoke() {
   });
   xhr.send();
 }
+
+
+//jQuery method:
+/*
+var url = 'http://api.icndb.com/jokes/random';
+
+var $button = $('#get-joke').click(function(){
+  getJoke();
+});
+
+var $paragraph = $('#joke');
+
+function getJoke() {
+  $.ajax ({
+    url: url,
+    method: 'GET',
+    success: function(res){
+      $paragraph.text(res.value.joke);
+    }
+  });
+}
+
+*/
